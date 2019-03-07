a571dd73f0839b76e3c810f868a6d5c67b3ad565
Andres <andresbio@hotmail.com>
2019-03-07 00:47:27 -0500

    lectura y escritura

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const fs=require("fs");
+const ops=require("./src/fileops");
+
+
+function read(){
+fs.readFile("./resources/number.txt","utf8",
+(err,texto)=>{
+    if(err) throw err;
+    const numbers=texto.split("\n");
+    console.log(numbers)
+    return ops.incrementValues(numbers);
+    //console.log(listanumeros);
+    
+    
+});
+}
+
+function write(x){
+    fs.writeFile("./resources/numbernew.txt",
+    x.join("\n"),
+    (error,result)=>{
+        if(error) throw error
+    })
+}
+
+async function ejecutar(){
+    console.log("escribiendo")
+    const listanumeros=await read()
+    write(listanumeros)
+
+    
+}
+ejecutar()
+
