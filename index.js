a88445bbddbf4124e4c8a3c433f944f5361b45d5
Andres <andresbio@hotmail.com>
2019-03-17 23:27:45 -0500

    ajuste de index

--- index.js
+++ index.js
@@ -2,33 +2,39 @@ const fs=require("fs");
 const ops=require("./src/fileops");
 
 
-function read(){
-fs.readFile("./resources/number.txt","utf8",
-(err,texto)=>{
-    if(err) throw err;
-    const numbers=texto.split("\n");
-    console.log(numbers)
-    return ops.incrementValues(numbers);
-    //console.log(listanumeros);
-    
-    
-});
-}
+  async function read(){
+      const file=await fs.readFile("./resources/number.txt","utf8")
+      /* async function (error,texto){
+            
+                const numbers=texto.split("\n");
+                console.log("metodo read"+numbers);
+                return await  ops.incrementValues(numbers);  
+        }) */
+} 
 
-function write(x){
-    fs.writeFile("./resources/numbernew.txt",
-    x.join("\n"),
-    (error,result)=>{
-        if(error) throw error
-    })
-}
+
+//function write(x){
+    //console.log(x)
+    //fs.writeFile("./resources/numbernew.txt",
+    //x.join("\n"),
+    //(error,result)=>{
+        //if(error) throw error
+    //})
+//}
 
 async function ejecutar(){
     console.log("escribiendo")
-    const listanumeros=await read()
-    write(listanumeros)
+    const listanumeros= await read()
+    console.log("medoto ejecutar"+listanumeros)
+    //write(listanumeros)
 
     
 }
-ejecutar()
+try{
+    ejecutar()
+
+}
+catch(e){
+    console.log(e)
+}
 
