45095d7fb88d7101ebaaf1b9617b97324764b444
Andresbiomedico <andresbio@hotmail.com>
2019-03-19 22:54:49 -0500

    ejercicio para leer y crear nuevo archivo

--- index.js
+++ index.js
@@ -2,39 +2,42 @@ const fs=require("fs");
 const ops=require("./src/fileops");
 
 
-  async function read(){
-      const file=await fs.readFile("./resources/number.txt","utf8")
-      /* async function (error,texto){
-            
-                const numbers=texto.split("\n");
-                console.log("metodo read"+numbers);
-                return await  ops.incrementValues(numbers);  
-        }) */
-} 
+ function read(){
+    const lecture= new Promise((resolve,reject)=>{
+        console.log("entre a la promesa ")
+            fs.readFile("./resources/number.txt","utf8",async function (error,texto){
+                if (error) reject(error)
+                text= await texto
+                const numbers=text.split("\n")
+                console.log("metodo read"+numbers)
+                resolve(ops.incrementValues(numbers))
+            })  
+        })
+    return lecture          
+    } 
 
-
-//function write(x){
-    //console.log(x)
-    //fs.writeFile("./resources/numbernew.txt",
-    //x.join("\n"),
-    //(error,result)=>{
-        //if(error) throw error
-    //})
-//}
+function write(x){
+    console.log(x)
+    fs.writeFile("./resources/numbernew.txt",x.join("\n"),
+    (error,result)=>{
+        if(error) throw  error
+    })
+}
 
 async function ejecutar(){
-    console.log("escribiendo")
+try {
+    console.log("escribiendo") 
     const listanumeros= await read()
     console.log("medoto ejecutar"+listanumeros)
-    //write(listanumeros)
-
+     write(listanumeros) 
+}
+    catch (error) {
+        console.log(`error catch parce ${error}`)
+    }  
+}
     
-}
-try{
-    ejecutar()
 
-}
-catch(e){
-    console.log(e)
-}
+ejecutar()
+
+
 
