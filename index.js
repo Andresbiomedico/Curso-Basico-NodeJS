84a5a45f2680c131d2d1b693ee75c08095cd6e6a
Andresbiomedico <andresbio@hotmail.com>
2019-03-20 22:06:59 -0500

    child process

---
+++ index.js
@@ -0,0 +1,14 @@
+const cp=require("child_process")
+
+function execCommand(command){
+    cp.exec(command,(err,stdout,stderr)=>{
+        if(err){
+            console.log(`Error: ${err}`)
+            return
+        }
+        if(stdout) console.log(`Standartd out: \n${stdout}`)
+        if(stderr) console.log(`Standartd err: \n${stderr}`)
+    })
+}
+//execCommand("ls")
+execCommand("node ./src/script --base=5")
